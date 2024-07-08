a27feff119ff69559890bf8242cc68d5d3dd0597
Rajan Jha <138964806+rajanjha9235@users.noreply.github.com>
2024-07-08 14:04:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+new TradingView.widget(
+    {
+        "autosize" : true,
+        "symbol" : "BINANCE:BTCUSDT",
+        "interval" : "240",
+        "timezone" : "Etc/Utc",
+        "theme" : "Dark",
+        "style" : "1",
+        "locale" : "en",
+        "toolbar_bg" : "#f1f3f6",
+        "enable_publishing" : false,
+        "withdateranges" : true,
+        "hide_side_toolbar" : false,
+        "allow_symbol_changes" : true,
+        "watchlist" : [
+            "BINANCE:BTCUSDT",
+            "BINANCE:ETHUSDT"
+        ],
+        "details" : true,
+        "hotlist" : true,
+        "calendar" : true,
+        "studies" : ["STD;SMA"],
+        "container_id" : "chart",
+        "show_popup_button" : true,
+        "popup_width" : "1000",
+        "popup_height" : "650",
+
+    }
+)
